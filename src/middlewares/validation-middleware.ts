1d2b8ffbfd9e3b51e1ba273dd0f4cbcf2911d38e
Matthew Laird <hvk500@gmail.com>
2020-10-24 22:51:48 +0200

    Fixed floating promise

--- src/middlewares/validation-middleware.ts
+++ src/middlewares/validation-middleware.ts
@@ -5,14 +5,15 @@ import HttpException from '../exceptions/http-exception';
 
 export default function validationMiddleware(type: any, skipMissingProperties = false): RequestHandler {
   return (req, res, next) => {
+    // tslint:disable-next-line: no-floating-promises
     validate(plainToClass(type, req.body), { skipMissingProperties: skipMissingProperties })
-    .then((errors: ValidationError[]) => {
-      if (errors.length > 0) {
-        const message = errors.map((error: ValidationError) => Object.values(error.constraints)).join(', ');
-        next(new HttpException(400, message));
-      } else {
-        next();
-      }
-    });
+      .then((errors: ValidationError[]) => {
+        if (errors.length > 0) {
+          const message = errors.map((error: ValidationError) => Object.values(error.constraints)).join(', ');
+          next(new HttpException(400, message));
+        } else {
+          next();
+        }
+      });
   };
 }
